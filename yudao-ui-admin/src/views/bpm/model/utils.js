a556afc62ec1bb85e5a67c0b291b8df90d97e8b3
wu-yiqiang <wu_yiqiang@outlook.com>
2023-01-07 16:57:02 +0800

    初始化

---
+++ yudao-ui-admin/src/views/bpm/model/utils.js
@@ -0,0 +1,8 @@
+export const templateXml = `
+<?xml version="1.0" encoding="UTF-8"?>
+        <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" id="Definitions_0u4gsqp" targetNamespace="http://bpmn.io/schema/bpmn" exporter="bpmn-js (https://demo.bpmn.io)" exporterVersion="6.1.1">
+          <bpmn:process id="Process_1d6bkiu" isExecutable="false" />
+          <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+            <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1d6bkiu" />
+          </bpmndi:BPMNDiagram>
+        </bpmn:definitions>`
